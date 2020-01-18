413d11a18c045a09c524c1a4be8afdf05ed21bdf
Skallywagg <lee.sam1492@gmail.com>
2020-01-18 13:42:02 -0700

    loops

--- Js1/javascript1.js
+++ Js1/javascript1.js
@@ -20,15 +20,41 @@
     
     //Objects {}
     
-//Loops
-
-//Arrays []
-
 //Create an array called dogNames and set it to an empty array
-let dogNames = [];
+    let dogNames = [];
 
 //with the push() method, push a few dog names into the dogNames array
-dogNames.push('otis', 'ziggy', 'riply');
+    dogNames.push('otis', 'ziggy', 'riply');
+    
+//Loops
+
+// for loops
+
+for(var i = 0; i < dogNames.length; i++) {
+    console.log(i)
+}
+
+// while loops
+// .map()
+dogNames.map((value, index) => {
+    console.log("index:", index)
+    console.log("value:", value)
+})
+// .filter()
+let dogs = dogNames.filter( (value, index) => {
+    if(index != 0) {
+      return value
+    }
+})
+console.log(dogs)
+// .forEach()
+dogNames.forEach((value, index) => {
+    console.log("index:", index)
+    console.log("value:", value)
+})
+
+
+//Arrays []
 
 
 
