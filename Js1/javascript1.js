d6677ebea8aff1f4c577ee67187fef944a6121ed
PwntasticKev <pwntastickevin@gmail.com>
2020-01-18 15:40:13 -0700

    added the function into the file

--- Js1/javascript1.js
+++ Js1/javascript1.js
@@ -15,11 +15,10 @@
     //Code Here
 
     
-//* * Functions ()
 	
-
+	
     
-//* * Objects {}
+	//* * Objects {}
 //  -- Objects are very similar to arrays.
 
 let dog = {}
@@ -32,53 +31,53 @@ dog.name = "Otis"
 // dog.age = 5;
 
 let otis = {
-  name: "otis",
-  age: 5
+	name: "otis",
+	age: 5
 }
 
 console.log(otis['age'])
 console.log( otis.age )
 
 let person = {
-    name: 'sam',
+	name: 'sam',
     lastName: 'lee',
     age: 27,
     weight: 150,
     hair: 'brown',
-  }
-  console.log(person.weight + 'lb')
-  
+}
+console.log(person.weight + 'lb')
+
 
 //dot notation
 console.log( otis.name )
 // bracket notation
 console.log( otis['name'] )
-    
+
 //Create an array called dogNames and set it to an empty array
-    let dogNames = [];
+let dogNames = [];
 
 //with the push() method, push a few dog names into the dogNames array
-    dogNames.push('otis', 'ziggy', 'riply');
-    
+dogNames.push('otis', 'ziggy', 'riply');
+
 //* * Loops & Arrays
 
 // for loops
 
 for(var i = 0; i < dogNames.length; i++) {
-    console.log(i)
+	console.log(i)
 }
 
 // while loops
 
 // .map()
 cars.map( (carName) => {
-    console.log(carName)
-  })
+	console.log(carName)
+})
 
 // .filter()
 let dogs = dogNames.filter( (value, index) => {
-    if(index != 0) {
-      return value
+	if(index != 0) {
+		return value
     }
 })
 console.log(dogs)
@@ -86,38 +85,50 @@ console.log(dogs)
 // .forEach()
 
 dogNames.forEach((value, index) => {
-    console.log("index:", index)
+	console.log("index:", index)
     console.log("value:", value)
 })
 
 let cars = ['subie', 'camaro', 'mustang5.0'];
 
 // for(var i = 0; i < cars.length; i++){
-//   console.log(i)
+	//   console.log(i)
+	
+	// }
+	
+	// * * Arrays and objects
+	
+	let cars = [];
+	
+	let mustang = {
+		name: "Betsy Stang",
+		year: 2012,
+		color: "red",
+		vin: 1230123012
+	}
+	
+	let camaro = {
+		name: "Betsy Maro",
+		year: 1995,
+		color: "Green",
+		vin: 128371238
+	}
+	
+	cars.push( camaro, mustang )
+	console.log( cars[0].name )
+	
+	
+	//* * Functions ()
 
-// }
-
-// * * Arrays and objects
-
-let cars = [];
-
-let mustang = {
-  name: "Betsy Stang",
-  year: 2012,
-  color: "red",
-  vin: 1230123012
-}
-
-let camaro = {
-  name: "Betsy Maro",
-  year: 1995,
-  color: "Green",
-  vin: 128371238
-}
-
-cars.push( camaro, mustang )
-console.log( cars[0].name )
-
-
-
-
+	function getCarName () {
+		for(var i = 0; i < cars.length; i++) {
+		  if(cars[i].name == 'Betsy Maro') {
+			return cars[i].name
+		  }
+		}
+	  }
+	  
+	  getCarName()
+	
+	
+	
