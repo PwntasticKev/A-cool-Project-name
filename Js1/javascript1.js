94e590cc16f937bb12499c220bcb1e76ec6d898a
Skallywagg <lee.sam1492@gmail.com>
2020-01-18 13:23:00 -0700

    arrays

--- Js1/javascript1.js
+++ Js1/javascript1.js
@@ -1,5 +1,34 @@
-hello
+// Variables
 
-Its kevin
-chocolate?!
-Whatre ya selling
+// var - global Scope
+// let - lexical scope
+// const - Cannot be changed
+
+//create a variable called dog and assign to be a STRING of dog
+    //code Here
+    let dog = 'dog';
+    dog = 'cat';
+    console.log(dog);
+
+
+// reassign dog to be a STRING of cat
+    //Code Here
+
+    
+    //Functions ()
+    
+    
+    //Objects {}
+    
+//Loops
+
+//Arrays []
+
+//Create an array called dogNames and set it to an empty array
+let dogNames = [];
+
+//with the push() method, push a few dog names into the dogNames array
+
+
+
+
