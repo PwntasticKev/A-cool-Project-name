3be3b6ab4dca24f42363d162c15a4c0f028e1c9f
PwntasticKev <pwntastickevin@gmail.com>
2020-01-18 19:00:56 -0700

    added homework folder

---
+++ Js1/StudyWork/Arrays.js
@@ -0,0 +1,17 @@
+// in your own words, what is an array?
+
+
+//create an array of numbers
+
+//loop over each array and add them up.
+
+
+//create an array of cs:go maps
+
+//! EXTRA CREDIT
+//create a variable called foundPlayers
+//CODE HERE
+
+//  you are in que searching for a map. you need 10 total people to join a map.
+// if the map is "office" and the foundPlayers variable is 10 return the string "ACCEPT"
+//CODE HERE
---
+++ Js1/StudyWork/Functions.js
@@ -0,0 +1,47 @@
+// in your own words, what is an array?
+
+// * * Create a function called getName that takes in two parameters; ( name1, name2 )
+
+// * * call the function with two arguments
+
+//  * * return a template string while using both of the variables you used as parameters --- SEE LINK
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+
+
+// * * Create a function that takes in 3 numbers as parameters and mutlplies them as a result. 
+
+
+// ! EXTRA CREDIT
+
+// Create an array of car objects
+
+// Create a function called getCar
+
+// getCar will loop through each car in the cars array by using the .map() method
+
+// return the carname of your choice.
+
+
+
+
+// ! EXTRA EXTRA credit
+
+// instead of using .map(), use the .filter() method to return any car != to a certain car of your choice. Exmaple: != mustang
+
+
+// ! ULTRA CREDIT
+
+//create a function called getCurrentHealth that takes three parameters, damageTaken, currentHp and headshot ( headshot will be passed a boolean )
+
+// return the currentHp minus damageTaken times 2 if headshot is true. 
+// if headshot is false we will take normal damage
+// if the currentHp is 0 equal or less, return the string "oh dear! you are dead"
+
+
+
+
+
+
+
+
+
